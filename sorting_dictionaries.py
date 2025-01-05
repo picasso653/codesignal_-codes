e557c31a7ce290e1ab5bf258382ffaac99296a95
Isaac Korankye Asante <xquisit31@gmail.com>
2025-01-05 20:20:54 +0000

    removed and replaced

--- sorting_dictionaries.py
+++
@@ -1,9 +0,0 @@
-def sort_scores(unsorted_scores):
-    # implement this
-    sorted_scores = sorted(unsorted_scores.items(), key=lambda x : x[1], reverse=True)
-    return sorted_scores
-    pass
-
-print(sort_scores({'Lora': 97, 'Jason': 99, 'Apple': 64}))
-print(sort_scores({'Vanguard': 220, 'Origin': 100, 'Eternal': 150, 'Reaper': 180}))
-print(sort_scores({'Charlie': 45, 'Tango': 70, 'Delta': 35}))
