2362cbc7ebd42b892aa4d206df270e756fb43e01
Isaac Korankye Asante <xquisit31@gmail.com>
2025-01-20 21:13:45 +0000

    new code

---
+++ Data Science/Intro to numpy/reshape.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+# 1D Numpy array
+np_array = np.array([1, 2, 3, 4])
+
+# TODO: Reshape np_array to a shape of (2, 2) and assign it to variable reshaped_array
+reshaped_array = np_array.reshape(2, 2)
+# Print the reshaped array
+print(reshaped_array)
