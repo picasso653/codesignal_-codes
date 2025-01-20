8ca942a69b0183ffa595e9523199e29bcdcb794f
Isaac Korankye Asante <xquisit31@gmail.com>
2025-01-20 21:13:26 +0000

    new code

---
+++ Data Science/Intro to numpy/numpy_multiplication.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# create a 1D numpy array
+np_array1 = np.array([10, 20, 30])
+
+# create another 1D numpy array
+np_array2 = np.array([2, 3, 5])
+
+# perform division operation on numpy arrays
+result_array = np_array1 * np_array2
+
+# print the resulting array
+print("Resulted Array:", result_array)
