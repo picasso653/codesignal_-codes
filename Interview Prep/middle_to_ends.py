e1cdb06c3d9c83b6a1df6c432739fbfa8db9b53d
Isaac Korankye Asante <xquisit31@gmail.com>
2025-02-11 18:35:29 +0000

    new code

---
+++ Interview Prep/middle_to_ends.py
@@ -0,0 +1,17 @@
+def solution(numbers):
+    new_order =[]
+    mid = len(numbers) // 2
+    if len(numbers) % 2 == 1:
+        left = mid - 1
+        right = mid + 1
+        new_order.append((numbers[mid], 0))
+    else:
+        left = mid - 1
+        right = mid
+    while left >= 0 and right <= len(numbers):
+        p = (numbers[left], numbers[right])
+        new_order.append(p)
+        left -= 1
+        right += 1
+    return new_order
+    pass
