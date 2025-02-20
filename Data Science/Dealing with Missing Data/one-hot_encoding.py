5ea7214534682f594c079c0604737248e8f682af
Isaac Korankye Asante <xquisit31@gmail.com>
2025-02-20 19:33:06 +0000

    new code

--- Data Science/Dealing with Missing Data/one-hot_encoding.py
+++ Data Science/Dealing with Missing Data/one-hot_encoding.py
@@ -9,7 +9,6 @@ titanic_df = sns.load_dataset('titanic')
 titanic_df['class_encoded'] = pd.factorize(titanic_df['class'])[0]
 print(titanic_df[['class', 'class_encoded']].head())
 
-# TODO: Apply One-Hot Encoding to the 'who' column
 encoded_df = pd.get_dummies(titanic_df['who'], prefix='is')
 titanic_df = pd.concat([titanic_df, encoded_df], axis=1)
 print(titanic_df[['is_man', "is_woman", 'is_child']].head())
